49826750dd13b7c2272ac80edc10a65637aa5e58
FrancoPappatella <franco_pappatella@hotmail.com>
2023-02-02 18:42:55 -0300

    feat(form): Se agregan estilos con bootstrap al formulario.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,5 @@
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (1, 'Franco', 'Pappatella', 'pappatella.francoariel@gmail.com', '2022-12-1');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (2, 'Juan', 'Perez', 'juanperez@gmail.com', '2022-12-7');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (3, 'Agustina', 'Gimenez', 'gimenezagustina@gmail.com', '2022-12-9');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES (4, 'Gloria', 'Ramirez', 'gloria_ramirez@hotmail.com', '2022-12-15');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Franco', 'Pappatella', 'pappatella.francoariel@gmail.com', '2022-12-1');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Juan', 'Perez', 'juanperez@gmail.com', '2022-12-7');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Agustina', 'Gimenez', 'gimenezagustina@gmail.com', '2022-12-9');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Gloria', 'Ramirez', 'gloria_ramirez@hotmail.com', '2022-12-15');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Ariel', 'Rodriguez', 'ari_rodriguez@hotmail.com', '2022-12-20');
