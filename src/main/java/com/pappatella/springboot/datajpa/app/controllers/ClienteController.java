6050f984a2ddd97a0083842a9659c49fb4fa5d35
FrancoPappatella <franco_pappatella@hotmail.com>
2023-02-05 19:18:27 -0300

    refactor(domain): La fecha de createAt se podrá agregar mediante el formulario y no con PrePersist

--- src/main/java/com/pappatella/springboot/datajpa/app/controllers/ClienteController.java
+++ src/main/java/com/pappatella/springboot/datajpa/app/controllers/ClienteController.java
@@ -1,7 +1,5 @@
 package com.pappatella.springboot.datajpa.app.controllers;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,9 +22,9 @@ public class ClienteController {
 	}
 	
 	@GetMapping("/form")
-	public String crear(Map<String, Object> model) {
+	public String crear(Model model) {
 		Cliente cliente = new Cliente();
-		model.put("cliente", cliente);
+		model.addAttribute("cliente", cliente);
 		return "form";
 	}
 	
--- src/main/java/com/pappatella/springboot/datajpa/app/models/entity/Cliente.java
+++ src/main/java/com/pappatella/springboot/datajpa/app/models/entity/Cliente.java
@@ -8,10 +8,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "clientes")
@@ -21,21 +25,22 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@NotEmpty
 	private String nombre;
+	@NotEmpty
 	private String apellido;
+	@NotEmpty
+	@Email
 	private String email;
 
+	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
 
 	private static final long serialVersionUID = 1L;
 
-	@PrePersist
-	public void prePersist() {
-		this.createAt = new Date();
-	}
-	
 	public Long getId() {
 		return id;
 	}
