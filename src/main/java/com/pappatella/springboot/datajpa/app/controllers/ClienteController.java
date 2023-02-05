c814b61d26a643668c54b01a385faae8efb85052
FrancoPappatella <franco_pappatella@hotmail.com>
2023-02-05 19:57:10 -0300

    feat(form): Se agregaron reglas de validación en el formulario

--- src/main/java/com/pappatella/springboot/datajpa/app/controllers/ClienteController.java
+++ src/main/java/com/pappatella/springboot/datajpa/app/controllers/ClienteController.java
@@ -1,8 +1,11 @@
 package com.pappatella.springboot.datajpa.app.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -20,17 +23,21 @@ public class ClienteController {
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "clientes";
 	}
-	
+
 	@GetMapping("/form")
 	public String crear(Model model) {
 		Cliente cliente = new Cliente();
 		model.addAttribute("cliente", cliente);
 		return "form";
 	}
-	
+
 	@PostMapping("/form")
-	public String guardar(Cliente cliente) {
-		clienteDao.save(cliente);
+	public String guardar(@Valid Cliente cliente, BindingResult result) {
+		if (result.hasErrors()) {
+			return "form";
+		}
+
+ 		clienteDao.save(cliente);
 		return "redirect:listar";
 	}
 }
